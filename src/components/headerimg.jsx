ee890772d241e5d6f6a3c94aae953bff4aae2023
Manoj Kumar Padhy <manojkumarpadhyms@gmail.com>
2024-02-18 20:45:22 +0530

    header completed

--- src/components/headerimg.jsx
+++ src/components/headerimg.jsx
@@ -5,12 +5,14 @@ import logo from '../assets/logo.jpeg'
 export default function Haderimg() {
   return (
     <div className='header_img_container'>
-    <img src={image} />
+    <img alt='header_img' src={image} />
     <div className='header_heading'>
         <h1>Partner With Us</h1>
         <p>Be our partner in just few steps and start Increasing your reach by gaining new customers.</p>
-        <img className='header_logo' src={logo}/>
     </div>
+<div>
+<img alt='header_logo' className='header_logo' src={logo}/>
+</div>
     </div>
   )
 }
